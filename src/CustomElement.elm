1b083c87369491915aaac8e328e90bd2c9099794
Anatoliy <mail@anatoliy.in>
2018-09-08 21:06:58 +0100

    Support rules and outline textfield style

--- src/CustomElement.elm
+++ src/CustomElement.elm
@@ -7,12 +7,14 @@ import Html exposing (Html, div, h3, text)
 import Json.Decode exposing (Value, decodeValue)
 import Json.Encode as Encode
 import Json.Form
+import Json.Form.Config exposing (Config)
 import Json.Schema.Definitions
 import Json.Value as JsonValue exposing (decoder)
 
 
 type alias Model =
     { form : Json.Form.Model
+    , config : Config
     , schema : Json.Schema.Definitions.Schema
     , editedValue : Maybe JsonValue.JsonValue
     }
@@ -23,18 +25,24 @@ init v =
     let
         schema =
             v
-                |> decodeValue (Json.Decode.at [ "schema" ] Json.Schema.Definitions.decoder)
+                |> decodeValue (Json.Decode.field "schema" Json.Schema.Definitions.decoder)
                 |> Result.mapError Debug.log
                 |> Result.withDefault Json.Schema.Definitions.blankSchema
 
+        config =
+            v
+                |> decodeValue (Json.Decode.field "config" Json.Form.Config.decoder)
+                |> Result.withDefault Json.Form.Config.defaultConfig
+
         value =
             v
-                |> decodeValue (Json.Decode.at [ "value" ] JsonValue.decoder)
+                |> decodeValue (Json.Decode.field "value" JsonValue.decoder)
                 |> Result.toMaybe
     in
-    { form = Json.Form.init schema value
+    { form = Json.Form.init config schema value
     , editedValue = value
     , schema = schema
+    , config = config
     }
         ! []
 
@@ -43,11 +51,24 @@ type Msg
     = JsonFormMsg Json.Form.Msg
     | ChangeValue Value
     | ChangeSchema Value
+    | ChangeConfig Value
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update message model =
     case message of
+        ChangeConfig v ->
+            case v |> decodeValue Json.Form.Config.decoder of
+                Ok config ->
+                    { model
+                        | config = config
+                        , form = model.form |> Json.Form.updateConfig config
+                    }
+                        ! []
+
+                Err _ ->
+                    model ! []
+
         ChangeSchema v ->
             let
                 schema =
@@ -57,7 +78,7 @@ update message model =
             in
             { model
                 | schema = schema
-                , form = Json.Form.init schema model.editedValue
+                , form = Json.Form.init model.config schema model.editedValue
             }
                 ! []
 
@@ -70,7 +91,7 @@ update message model =
             in
             { model
                 | editedValue = value
-                , form = Json.Form.init model.schema value
+                , form = Json.Form.init model.config model.schema value
             }
                 ! []
 
@@ -120,9 +141,13 @@ port value : Value -> Cmd msg
 port schemaChange : (Value -> msg) -> Sub msg
 
 
+port configChange : (Value -> msg) -> Sub msg
+
+
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.batch
         [ valueChange ChangeValue
         , schemaChange ChangeSchema
+        , configChange ChangeConfig
         ]
--- src/custom-element.js
+++ src/custom-element.js
@@ -2,11 +2,20 @@ const Elm = require('./JsonFormCustomElement');
 const css = require('../../json-form/stylesheets/standalone.css').toString();
 
 
+function readAttribute(node, name, defValue) {
+    try {
+        return JSON.parse(node.getAttribute(name));
+    } catch (e) {
+        return defValue;
+    }
+}
+
+
 customElements.define('json-form',
     class extends HTMLElement {
 
         static get observedAttributes() {
-            return ['value', 'schema'];
+            return ['value', 'schema', 'config'];
         }
 
         constructor() {
@@ -20,10 +29,18 @@ customElements.define('json-form',
             shadowRoot.appendChild(appStyles);
             shadowRoot.appendChild(appRoot);
 
-            const json = this.getAttribute('value');
-            const schema = JSON.parse(this.getAttribute('schema'));
-            const value = JSON.parse(json);
-            const app = Elm.JsonFormCustomElement.embed(appRoot, { schema, value });
+            this._schema = readAttribute(this, 'schema', {});
+            this._value = readAttribute(this, 'value', null);
+            this._config = readAttribute(this, 'config');
+
+        }
+
+        connectedCallback() {
+            const app = Elm.JsonFormCustomElement.embed(appRoot, {
+                schema: this._schema,
+                value: this._value,
+                config: this._config
+            });
             this.app = app;
             this.muteAttributeChange = false;
 
@@ -41,12 +58,28 @@ customElements.define('json-form',
                 return;
             }
 
+            if (oldValue === newValue) {
+                return;
+            }
+
             switch (name) {
                 case 'value':
-                    this.app.ports.valueChange.send(JSON.parse(newValue));
+                    this._value = JSON.parse(newValue);
+                    if (this.app) {
+                        this.app.ports.valueChange.send(this._value);
+                    }
                     break;
                 case 'schema':
-                    this.app.ports.schemaChange.send(JSON.parse(newValue));
+                    this._schema = JSON.parse(newValue);
+                    if (this.app) {
+                        this.app.ports.schemaChange.send(this._schema);
+                    }
+                    break;
+                case 'config':
+                    this._config = JSON.parse(newValue);
+                    if (this.app) {
+                        this.app.ports.configChange.send(this._config);
+                    }
                     break;
             }
         }
